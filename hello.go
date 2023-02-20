31c2dee8ef9442803a9f72e946e4710a944f994a
Thiago Alves Pereira <thiago-alvesp@live.com>
2023-02-20 10:38:56 -0300

    Finalizado a estrutura do progama

--- hello.go
+++ hello.go
@@ -2,15 +2,50 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 	"os/user"
 )
 
 func main() {
 	exibeIntroducao()
+
+	for {
+		exibeMenu()
+		comando := leComando()
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			exibirLogs()
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheco este comando")
+			os.Exit(-1)
+		}
+	}
 }
 
 func iniciarMonitoramento() {
-	fmt.Printf("Monitorando...")
+	fmt.Println()
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br/"
+	fmt.Println()
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
+	fmt.Println()
+}
+
+func exibirLogs() {
+	fmt.Println()
+	fmt.Println("Exibindo Logs...")
+	fmt.Println()
 }
 
 func exibeIntroducao() {
@@ -26,4 +61,12 @@ func exibeMenu() {
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi ", comandoLido)
+
+	return comandoLido
 }
